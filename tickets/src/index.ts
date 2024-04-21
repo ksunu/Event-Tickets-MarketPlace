f6fc85d9abf7515f1190ece568d54c4727a0cc9d
ksunu <soonk@outlook.com>
2024-04-21 10:23:03 +0900

    implemented nats with created and updated ticket events

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose'
-
 import { app } from './app'
+import { natsWrapper } from './nats-wrapper'
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined')
   if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined')
 
   try {
+    await natsWrapper.connect('ticketing', 'asdf', 'http://nats-srv:4222')
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!')
+      process.exit()
+    })
+    process.on('SIGNINT', () => natsWrapper.client.close())
+    process.on('SIGNTERM', () => natsWrapper.client.close())
+
     await mongoose.connect(process.env.MONGO_URI)
     console.log('Connected to MongoDb')
   } catch (err) {
