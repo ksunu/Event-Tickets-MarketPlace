c0c120433e5a62653e9e01fe6bf77150c5e29cbb
ksunu <soonk@outlook.com>
2024-04-21 10:42:12 +0900

    fixed tests mocking nats

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -27,7 +27,7 @@ router.post('/api/tickets', requireAuth, [
     })
     await ticket.save()
 
-    new TicketCreatedPublisher(natsWrapper.client).publish({
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
