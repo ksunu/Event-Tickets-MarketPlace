e627101ca7aa2e8a6f9882b1efa368a17421daf0
ksunu <soonk@outlook.com>
2024-04-23 18:46:23 +0900

    refactor find previous event version

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -17,6 +17,7 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc
+  findByEvent(event: { id: string, version: number }): Promise<TicketDoc | null>
 }
 
 const ticketSchema = new mongoose.Schema({
@@ -46,6 +47,12 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
     _id: attrs.id,
     title: attrs.title,
     price: attrs.price
+  })
+}
+ticketSchema.statics.findByEvent = (event: { id: string, version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1
   })
 }
 ticketSchema.methods.isReserved = async function() {
